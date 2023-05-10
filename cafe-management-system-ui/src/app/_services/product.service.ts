ccd0c444380f11449f05d62a6e0ef9f361f384ee
Mayank <mayankshah046@gmail.com>
2023-05-10 18:37:06 +0530

    commit
    Date: 10-05-2023

---
+++ cafe-management-system-ui/src/app/_services/product.service.ts
@@ -0,0 +1,74 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Product } from '../_model/product.model';
+import { OrderDetails } from '../_model/order-details.model';
+import { MyOrderDetails } from '../_model/order.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  public addProduct(product: FormData) {
+    return this.httpClient.post<Product>("http://localhost:9090/addNewProduct", product);
+  }
+
+  public getAllProducts(pageNumber: string | number, searchKeyword: string = "") {
+    return this.httpClient.get<Product[]>("http://localhost:9090/getAllProducts?pageNumber="+pageNumber+"&searchKey="+searchKeyword);
+  }
+
+  public getProductDetailsById(productId: any) {
+    return this.httpClient.get<Product>("http://localhost:9090/getProductDetailsById/" + productId);
+  }
+
+  public deleteProduct(productId: any) {
+    return this.httpClient.delete("http://localhost:9090/deleteProductDetails/" + productId);
+  }
+
+  public getProductDetails(isSingleProductCheckout: any, productId: any) {
+    return this.httpClient.get<Product[]>("http://localhost:9090/getProductDetails/" + isSingleProductCheckout + "/" + productId);
+  }
+
+  public addToCart(productId: any) {
+    return this.httpClient.get("http://localhost:9090/addToCart/" + productId);
+  }
+
+
+  public placeOrder(orderDetails: OrderDetails, isCartCheckout: string | undefined) {
+    return this.httpClient.post("http://localhost:9090/placeOrder/"+isCartCheckout, orderDetails);
+  }
+
+
+public getCartDetails() {
+  return this.httpClient.get("http://localhost:9090/getCartDetails");
+}
+
+public deleteCartItem(cartId: string) {
+  return this.httpClient.delete("http://localhost:9090/deleteCartItem/"+cartId);
+}
+
+public getMyOrders(): Observable<MyOrderDetails[]> {
+  return this.httpClient.get<MyOrderDetails[]>("http://localhost:9090/getOrderDetails");
+}
+
+public getAllOrderDetailsForAdmin(status: string): Observable<MyOrderDetails[]> {
+  return this.httpClient.get<MyOrderDetails[]>("http://localhost:9090/getAllOrderDetails/"+status);
+}
+
+public markAsDelivered(orderId: string) {
+  return this.httpClient.get("http://localhost:9090/markOrderAsDelivered/"+orderId)
+}
+
+public createTransaction(amount: any) {
+  return this.httpClient.get("http://localhost:9090/createTransaction/"+amount);
+}
+
+
+
+  }
+
+
+
---
+++ cafe-management-system-ui/src/app/home/home.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../_services/product.service';
+import { Product } from '../_model/product.model';
+import { ImageProcessingService } from '../image-processing-service.service';
+import { map } from 'rxjs/operators';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit{
+  productDetails: Product[] = [];
+  pageNumber: number = 0;
+  showLoadButton = false;
+  
+  constructor(private productService: ProductService,
+    private imageProcessingService: ImageProcessingService,
+    private router: Router) {}
+  ngOnInit(): void {
+   this.getAllProducts();
+  }
+
+
+  searchByKeyword(searchkeyword: any) {
+console.log(searchkeyword);
+this.pageNumber = 0;
+this.productDetails = [];
+this.getAllProducts(searchkeyword);
+  }
+
+
+  public getAllProducts(searchKey: string = "") {
+    this.productService.getAllProducts(this.pageNumber, searchKey)
+    .pipe(
+      map((x: Product[], i) => x.map((product: Product) => this.imageProcessingService.createImages(product)))
+    )
+    .subscribe(
+      (resp: Product[]) => {
+        console.log(resp);
+        if(resp.length == 4) {
+          this.showLoadButton = true;
+        } else {
+          this.showLoadButton = false;
+        }
+        resp.forEach(p => this.productDetails.push(p));
+      }, (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    );
+  }
+
+  public loadMoreProduct() {
+    this.pageNumber = this.pageNumber + 1;
+    this.getAllProducts();
+  }
+
+  showProductDetails(productId: any) {
+    this.router.navigate(['/productViewDetails', {productId: productId}]);
+  }
+
+  
+}
